5ea6f995412881ae18d4ad1f369e1c6060aea487
Rene Hampölz <service@hampoelz.net>
2020-03-22 17:03:00 +0100

    Change stream server domain

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function (e) {
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('managed-21b453c0b63f7138bf3701bee37eee11e5ed2716').then(function (cache) {
+                    caches.open('managed-a561342f21dab8986bb86dbde6ae76d9e6f7fb69').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
@@ -38,4 +38,4 @@ self.addEventListener('fetch', event => {
 });
 
 
-// managed-21b453c0b63f7138bf3701bee37eee11e5ed2716
+// managed-a561342f21dab8986bb86dbde6ae76d9e6f7fb69
